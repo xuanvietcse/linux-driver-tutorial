0a5169ab12173673639c8d62d4e912f75869edb4
Hoang Xuan Viet <hxv1305@gmail.com>
2025-07-20 22:41:29 +0700

    update: use GPIO pins in a Linux Driver

---
+++ 03_Gpio_Ctrl/gpioctrl.c
@@ -0,0 +1,55 @@
+#include <linux/gpio/consumer.h>
+#include <linux/init.h>
+#include <linux/module.h>
+
+static struct gpio_desc *led, *button;
+
+#define IO_LED (21)
+#define IO_BUTTON (22)
+#define IO_OFFSET (512)
+
+static int __init my_init(void) {
+  int status;
+
+  led = gpio_to_desc(IO_LED + IO_OFFSET);
+  if (!led) {
+    printk("gpioctrl - Error getting pin %d\n", IO_LED);
+    return -ENODEV;
+  }
+
+  button = gpio_to_desc(IO_BUTTON + IO_OFFSET);
+  if (!button) {
+    printk("gpioctrl - Error getting pin %d\n", IO_BUTTON);
+    return -ENODEV;
+  }
+
+  status = gpiod_direction_output(led, 0);
+  if (status)  // Not 0
+  {
+    printk("gpioctrl - Error setting pin %d to output\n", IO_LED);
+    return status;
+  }
+
+  status = gpiod_direction_input(button);
+  if (status)  // Not 0
+  {
+    printk("gpioctrl - Error setting pin %d to input\n", IO_BUTTON);
+    return status;
+  }
+
+  gpiod_set_value(led, 1);
+  printk("gpioctrl - LED is ON\n");
+
+  printk("gpioctrl - Button is %s pressed\n",
+         gpiod_get_value(button) ? "" : "NOT");
+  return 0;
+}
+
+static void __exit my_exit(void) { printk("Goodbye - Goodbye Kernel!\n"); }
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Hoang Xuan Viet <hxv1305@gmail.com>");
+MODULE_DESCRIPTION("An example for using GPIOs without the device tree");
